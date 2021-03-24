e911fce69f94e0c9226dfbc54a33145b8975bbbf
Wojciech Koziol <koziol.wojciech.poczta@gmail.com>
2021-03-24 21:22:16 +0100

    added tests foe exceptions

--- BookWebTests/UserServiceTests.cs
+++ BookWebTests/UserServiceTests.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookWebDotNet.Domain.Exceptions;
 using Xunit;
 
 namespace BookWebTests
@@ -23,17 +24,11 @@ namespace BookWebTests
         }
 
         [Fact]
-        public async Task GetUserAsync_ShouldReturnUser_WhenUserExists()
+        public async Task GetUserAsync_ShouldReturnUser_WhenUserExistsWithId()
         {
             //Arrange
             var guid = Guid.NewGuid();
-            var data = GenerateUsers(guid).AsQueryable();
-            var mockSet = Substitute.For<DbSet<User>, IQueryable<User>>();
-
-            ((IQueryable<User>)mockSet).Provider.Returns(data.Provider);
-            ((IQueryable<User>)mockSet).Expression.Returns(data.Expression);
-            ((IQueryable<User>)mockSet).ElementType.Returns(data.ElementType);
-            ((IQueryable<User>)mockSet).GetEnumerator().Returns(data.GetEnumerator());
+            var mockSet = GenerateUserMockSet(guid);
 
             _userRepo.Users.Returns(mockSet);
 
@@ -44,11 +39,85 @@ namespace BookWebTests
             userDto.UserId.Should().Be(guid);
         }
 
+        [Fact]
+        public async Task GetUserAsync_ShouldThrowException_WhenUserNotExistsWithId()
+        {
+            //Arrange
+            var guid = Guid.NewGuid();
+            var mockSet = GenerateUserMockSet(null);
+        
+            _userRepo.Users.Returns(mockSet);
+        
+            //Act
+            Func<Task> act = async () =>
+            {
+                await _sut.GetUserAsync(guid);
+            };
+            //Assert
+
+            await act
+                .Should()
+                .ThrowAsync<EntityNotFoundException>()
+                .WithMessage($"Couldn\'t find user with id = {guid}");
+        }
+
+
+        [Fact]
+        public async Task GetUserAsync_ShouldReturnUser_WhenUserExistsWithEmail()
+        {
+            //Arrange
+            const string email = "test@test.pl";
+            var mockSet = GenerateUserMockSet(Guid.NewGuid());
+
+            _userRepo.Users.Returns(mockSet);
+
+            //Act
+            var userDto = await _sut.GetUserAsync(email);
+            //Assert
+            userDto.Should().NotBeNull();
+            userDto.Email.Should().Be(email);
+        }
+
+        [Fact]
+        public async Task GetUserAsync_ShouldThrowException_WhenUserNotExistsWithEmail()
+        {
+            //Arrange
+            const string email = "tes@ttttt.op";
+            var mockSet = GenerateUserMockSet(null);
+
+            _userRepo.Users.Returns(mockSet);
+
+            //Act
+            Func<Task> act = async () =>
+            {
+                await _sut.GetUserAsync(email);
+            };
+            //Assert
+
+            await act
+                .Should()
+                .ThrowAsync<EntityNotFoundException>()
+                .WithMessage($"Couldn\'t find user with email = {email}");
+        }
+
+        private static DbSet<User> GenerateUserMockSet(Guid? guid)
+        {
+            var data = GenerateUsers(guid).AsQueryable();
+            var mockSet = Substitute.For<DbSet<User>, IQueryable<User>>();
+
+            ((IQueryable<User>)mockSet).Provider.Returns(data.Provider);
+            ((IQueryable<User>)mockSet).Expression.Returns(data.Expression);
+            ((IQueryable<User>)mockSet).ElementType.Returns(data.ElementType);
+            ((IQueryable<User>)mockSet).GetEnumerator().Returns(data.GetEnumerator());
+            return mockSet;
+        }
+
+
         public static IEnumerable<User> GenerateUsers(Guid? id)
         {
             return new List<User>
             {
-                new User
+                new()
                 {
                     Email = "test@test.pl",
                     IsAdmin = false,
@@ -57,7 +126,7 @@ namespace BookWebTests
                     Surname = "Doe",
                     UserId = id ?? Guid.NewGuid()
                 },
-                new User
+                new ()
                 {
                     Email = "test2@tes2t.pl",
                     IsAdmin = false,
